1466ad0a280d8e56198c7fd5799fcfc46f0fecf7
Ivanovik Lucas <lucasarielivanovik@gmail.com>
2025-09-23 09:52:32 -0300

    primerta-etapa-casi-terminada

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,29 +1,41 @@
 import "./ItemListContainer.css";
+import adidasImg from "../assets/imagenes/adidas-5418998_640.jpg";
+import zapatillasNike from "../assets/imagenes/zapatillasNike.png";
+
+
 function ItemListContainer() {
   const Zapatillas = {
     Adidas: {
-      title: "Zapatillas Adidas Crazyflight 6 Mid",
+      title: "Zapatillas Adidas Crazyflight 6 Md",
       price: "7000",
-      img: "https://www.freepik.es/foto-gratis/nuevo-par-zapatillas-blancas-aisladas-blanco_21124156.htm#fromView=search&page=1&position=4&uuid=3d3da048-92a3-426f-8f23-176b7589d441&query=zapatillas+adidas"
+      img: adidasImg   // 👉 acá uso la variable importada
     },
     Nike: {
       title: "Zapatillas Nike Zoom Fly 6",
       price: "15000",
-      img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/12345/nike-zoom-fly-6.jpg"
+      img: zapatillasNike
     }
   };
 
   return (
     <div className="contenedor-productos">
       <div className="tarjeta-producto">
-        <img src={Zapatillas.Adidas.img} className="producto-img" alt={Zapatillas.Adidas.title} />
+        <img
+          src={Zapatillas.Adidas.img}
+          className="producto-img"
+          alt={Zapatillas.Adidas.title}
+        />
         <h2 className="producto-title">{Zapatillas.Adidas.title}</h2>
         <p className="producto-price">${Zapatillas.Adidas.price}</p>
         <button className="producto-button">Agregar al carrito</button>
       </div>
 
       <div className="tarjeta-producto">
-        <img src={Zapatillas.Nike.img} className="producto-img" alt={Zapatillas.Nike.title} />
+        <img
+          src={Zapatillas.Nike.img}
+          className="producto-img"
+          alt={Zapatillas.Nike.title}
+        />
         <h2 className="producto-title">{Zapatillas.Nike.title}</h2>
         <p className="producto-price">${Zapatillas.Nike.price}</p>
         <button className="producto-button">Agregar al carrito</button>
@@ -33,4 +45,3 @@ function ItemListContainer() {
 }
 
 export default ItemListContainer;
-
