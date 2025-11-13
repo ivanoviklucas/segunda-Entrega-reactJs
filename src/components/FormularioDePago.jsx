feba95984ffa3b49a1b51aeaca967fe1b13b9cb0
Ivanovik Lucas <lucasarielivanovik@gmail.com>
2025-11-13 01:11:56 -0300

    formulario con logica agregada

--- src/components/FormularioDePago.jsx
+++ src/components/FormularioDePago.jsx
@@ -1,12 +1,28 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./FormularioDePago.css";
 import { themeContext } from "./ccontext.jsx";
 import Swal from "sweetalert2";
+import { Phone } from "lucide-react";
 export default function FormularioDePago() {
+  const [FormData, SetFormData] = useState(
+    { userName: "",
+      email: "",
+      Phone: "",
+
+    }
+  )
   const { confirmarCompra } = useContext(themeContext);
   function handlerSumit(evento) {
     evento.preventDefault();
-    confirmarCompra();
+    confirmarCompra()
+
+  }
+  function handlerChange(evento){
+    const inputName = evento.target.name;
+    const value = evento.target.value;
+    const newFormData = {...FormData}
+      newFormData[inputName] = value
+      SetFormData(newFormData)
   }
 
   return (
@@ -16,16 +32,16 @@ export default function FormularioDePago() {
           <h3>Ingresa tus datos personas</h3>
           <label>
             Nombre y Apellido
-            <input placeholder="ingrese su nombre"></input>
+            <input onChange={handlerChange} name="userName" placeholder="ingrese su nombre" required type="text" ></input>
           </label>
 
           <label htmlFor="">
             @gmail
-            <input placeholder="ingrese su cuenta de gmail"></input>
+            <input onChange={handlerChange} name="email" placeholder="ingrese su cuenta de gmail" required type="email" ></input>
           </label>
           <label htmlFor="">
             Telefono
-            <input placeholder="ingrese su telefono de contacto"></input>
+            <input onChange={handlerChange} name="Phone" placeholder="ingrese su telefono de contacto" required type="tel" ></input>
           </label>
         </div>
         <button className="btn-comprar-carrito" type="submit">
